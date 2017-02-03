2a6caa00fbe529197cb5ff805f780a1419af37de
bwang8482 <wang940822@gmail.com>
2017-02-02 22:52:41 -0600

    375

--- Google/17_Letter_Combinations of_a_Phone_Number.py
+++ Google/17_Letter_Combinations of_a_Phone_Number.py
@@ -13,6 +13,27 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 """Solution:
 """
 
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits: return []
+        self.ans = []
+        self.maps = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        self.bfs('', digits)
+        return self.ans
+
+    def bfs(self, string, digits):
+        if not digits:
+            self.ans.append(string)
+            return
+        for char in self.maps[digits[0]]:
+            self.bfs(string + char, digits[1:])
+            
+        
+
 
 
 
